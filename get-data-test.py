f1e4e3c0789feec5dfdb4e3fafdfbf47f5ffb57d
wzj042 <wzj042@qq.com>
2024-07-16 18:12:44 +0800

    添加保存测试结果的函数

--- get-data-test.py
+++ get-data-test.py
@@ -1,13 +1,17 @@
 from dotenv import load_dotenv, find_dotenv
-import os
+import os, json
 import requests
+from datetime import datetime
 
 class API:
     BASE_URL = f"https://afdian.net/api"
     
     @classmethod
     def post_detail(cls, post_id):
-        return f"{cls.BASE_URL}/post/get-detail?post_id={post_id}"
+        return {
+            "url": f"{cls.BASE_URL}/post/get-detail?post_id={post_id}",
+            "name": "get-post-detail"
+        }
 
 TEST_POST_ID = '6d117aca40ba11efa9795254001e7c00'
 
@@ -26,11 +30,28 @@ headers = {
 }
 
 
-url = API.post_detail(TEST_POST_ID)
+post_detail = API.post_detail(TEST_POST_ID)
 
+"""
+    保存 API 测试结果
+"""
+def save_api_response(api_name, response_data, base_path="res-example"):
+    # 确保基于API名称的路径存在
+    full_path = os.path.join(base_path, api_name)
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
+    
+    time_str = datetime.now().strftime("%Y%m%d-%H%M%S")
+    file_path = os.path.join(full_path, f"{time_str}.json")
+    
+    with open(file_path, 'w', encoding='utf-8') as file:
+        json.dump(response_data, file, ensure_ascii=False, indent=4)
+    
+    print(f"Data saved to {file_path}")
 
+response = requests.get(url=post_detail["url"], headers=headers, proxies=proxies)
 
-response = requests.get(url=url, headers=headers, proxies=proxies)
+save_api_response(post_detail["name"], response.json())
 
 print(response.status_code)
 print()
