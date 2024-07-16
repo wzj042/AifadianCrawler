9abe88c93b386fd67d805694cfecda91702b013f
wzj042 <wzj042@qq.com>
2024-07-16 18:35:07 +0800

    简单封装一下

--- get-data-test.py
+++ get-data-test.py
@@ -3,6 +3,7 @@ import os, json
 import requests
 from datetime import datetime
 
+
 class API:
     BASE_URL = f"https://afdian.net/api"
     
@@ -22,26 +23,23 @@ proxies = {
 
 load_dotenv(find_dotenv(), override=True)
 
-cookies = os.getenv('COOKIES')
-
+cookies = "os.getenv('COOKIES')"
 
 headers = {
     'Cookie': cookies
 }
 
 
-post_detail = API.post_detail(TEST_POST_ID)
 
 """
     保存 API 测试结果
 """
 def save_api_response(api_name, response_data, base_path="res-example"):
-    # 确保基于API名称的路径存在
     full_path = os.path.join(base_path, api_name)
     if not os.path.exists(full_path):
         os.makedirs(full_path)
     
-    time_str = datetime.now().strftime("%Y%m%d-%H%M%S")
+    time_str = datetime.now().strftime("%Y%m%d-%H-%M-%S")
     file_path = os.path.join(full_path, f"{time_str}.json")
     
     with open(file_path, 'w', encoding='utf-8') as file:
@@ -49,11 +47,25 @@ def save_api_response(api_name, response_data, base_path="res-example"):
     
     print(f"Data saved to {file_path}")
 
-response = requests.get(url=post_detail["url"], headers=headers, proxies=proxies)
 
-save_api_response(post_detail["name"], response.json())
+"""
+    测试 API 请求
+"""
+def test_api(api, headers=headers, proxies=proxies):
 
-print(response.status_code)
-print()
-print(response.json())
+    response = requests.get(url=api["url"], headers=headers, proxies=proxies)
+
+    save_api_response(api["name"], response.json())
+
+    print("链接成功" if response.status_code == 200 else "链接失败") 
+    try:
+        if response.json()["data"]["post"]["has_right"] == 0:
+            print("当前内容未订阅或 Cookies 已过期")
+    except KeyError:
+        print("当前内容格式错误或代码已过期")
+
+
+post_detail = API.post_detail(TEST_POST_ID)
+test_api(post_detail)
+
 
